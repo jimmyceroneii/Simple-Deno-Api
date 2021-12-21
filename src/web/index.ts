94e2032ff9127313213731c07f937accf9ec687a
jimmyceroneii <jimmy.cerone@atomicobject.com>
2021-12-21 09:38:35 -0500

    basic route

--- src/web/index.ts
+++ src/web/index.ts
@@ -8,13 +8,24 @@ interface CreateServerDependencies {
 
 export const createServer = async (config: CreateServerDependencies) => {
   const handler = (request: Request) => {
-    const body = `Your user-agent is:\n\n${
-      request.headers.get(
-        "user-agent",
-      ) ?? "Unknown"
-    }`;
+    console.log("RequestURL: ", request.url);
+    if (
+      request.url ===
+        `http://localhost:${config.configuration.port}/api/plants` &&
+      request.method === "GET"
+    ) {
+      const body = JSON.stringify({ plants: [] });
 
-    return new Response(body, { status: 200 });
+      return new Response(body, { status: 200 });
+    } else {
+      const body = `Your user-agent is:\n\n${
+        request.headers.get(
+          "user-agent",
+        ) ?? "Unknown"
+      }`;
+
+      return new Response(body, { status: 200 });
+    }
   };
 
   const server = new Server({ handler });
